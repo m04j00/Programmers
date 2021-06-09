9164ccb9886e71967f92d1bd36c8133eb83130d7
LEE MINJI <m04j00@gmail.com>
2021-06-09 17:33:09 +0900

    day 8

--- Level1/약수의 합/no15.java
+++ Level1/약수의 합/no15.java
@@ -1,13 +1,13 @@
-#include <stdio.h>
-#include <stdbool.h>
-#include <stdlib.h>
-
-int solution(int n) {
-    int answer = 0;
-    
-    for(int i = 1; i <= n; i++){
-        if(n % i == 0) answer += i;
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        for(int i = 1; i <= n; i++){
+            if(n % i == 0) {
+                answer += i;
+            }
+        }
+        
+        return answer;
     }
-    
-    return answer;
 }
