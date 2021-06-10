022d4fc798dcea8a9876ce636fcaa9e2fad5d63d
LEE MINJI <m04j00@gmail.com>
2021-06-10 15:45:34 +0900

    day 9

---
+++ Level1/자릿수 더하기/no17.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int n) {
+    int answer = 0;
+    int num = n;
+    
+    while(num != 0){
+        answer += num % 10;
+        num /= 10;
+    }
+    
+    return answer;
+}
