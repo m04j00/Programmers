6a781f65a406833b878ad073c8eb8460cef6b684
LEE MINJI <m04j00@gmail.com>
2021-06-12 17:55:22 +0900

    day 11

---
+++ Level1/콜라츠 추측/no21.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int num) {
+    int answer = 0;
+    long n = num;
+    
+    while(answer <= 500){
+        
+        if(n == 1) return answer;
+        
+        n = n % 2 == 0 ? n / 2 : n * 3 + 1;
+        
+        answer++;
+    }
+    
+    return -1;
+}
