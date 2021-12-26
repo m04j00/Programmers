bf446878ab9c588fbdcb4921a8069568a0cf416b
LEE MINJI <m04j00@gmail.com>
2021-12-26 22:57:22 +0900

    숫자의 표현

--- Level2/숫자의 표현/js.js
+++ Level2/숫자의 표현/js.js
@@ -2,13 +2,9 @@ function solution(n) {
     var answer = 1;
     for (let k = 1; k <= n / 2; k++) {
         let sum = 0;
-        for (let i = k; i <= n; i++) {
-            sum += i;
-            if (sum == n) {
-                answer++;
-                break;
-            } else if (sum > n) break;
-        }
+        let i = k;
+        while (sum < n) sum += i++;
+        if (sum == n) answer++;
     }
     return answer;
 }
