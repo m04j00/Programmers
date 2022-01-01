493b83b9002359903fd8c339f1339b4414ef4f3e
LEE MINJI <m04j00@gmail.com>
2022-01-01 21:01:52 +0900

    방문 길이 - 코드 수정

--- Level2/방문 길이/js.js
+++ Level2/방문 길이/js.js
@@ -1,7 +1,7 @@
 function solution(dirs) {
     var answer = 0;
     let p = [0, 0];
-    const visit = new Set();
+    const visit = [];
     for (let i of dirs) {
         const cur_p = p.slice();
 
@@ -12,13 +12,39 @@ function solution(dirs) {
 
         const st = p.join('') + cur_p.join('');
         const nd = cur_p.join('') + p.join('');
-        if (!visit.has(st) && st != nd) {
-            visit.add(st);
-            visit.add(nd);
+        if (!visit.includes(st) && st != nd) {
+            //console.log('akak');
+            visit.push(st);
+            visit.push(nd);
         }
 
     }
-    // console.log(...visit);
-    answer = visit.size / 2;
+    //console.log(...visit);
+    answer = visit.length / 2;
     return answer;
 }
+
+// function solution(dirs) {
+//     var answer = 0;
+//     let p = [0, 0];
+//     const visit = new Set();
+//     for (let i of dirs) {
+//         const cur_p = p.slice();
+
+//         if (i == 'U' && p[1] < 5) p[1]++;
+//         else if (i == 'D' && p[1] > -5) p[1]--;
+//         else if (i == 'R' && p[0] < 5) p[0]++;
+//         else if (i == 'L' && p[0] > -5) p[0]--;
+
+//         const st = p.join('') + cur_p.join('');
+//         const nd = cur_p.join('') + p.join('');
+//         if (!visit.has(st) && st != nd) {
+//             visit.add(st);
+//             visit.add(nd);
+//         }
+
+//     }
+//     // console.log(...visit);
+//     answer = visit.size / 2;
+//     return answer;
+// }
